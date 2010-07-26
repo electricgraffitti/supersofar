7721c916d6f68703dfbc086c0dc482d7584049bd
Bob Hanson <Bob@super17.local>
2010-07-25 17:43:22 -0700

    removing the protected assets

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -24,6 +24,6 @@ class Asset < ActiveRecord::Base
                     :url => "/attachments/:id/:style_:basename.:extension",
                     :path => ":rails_root/public/attachments/:id/:style_:basename.:extension"
   # Attrs
-  attr_protected :attachment_file_name, :attachment_content_type, :attachment_file_size
+  # attr_protected :attachment_file_name, :attachment_content_type, :attachment_file_size
   
 end
